b9dac16ee33f4b0ff966a8ded5cb2aa72b73dc4e
LillanEriksson <nailsbylillan@hotmail.com>
2020-10-23 22:34:09 +0100

    Added the objects and arrays with data for characters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
-import React from 'react'
+/* eslint-disable spaced-comment */
+/* eslint-disable linebreak-style */
+import React from 'react';
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+	const randomSelector = (array) => {
+		return array[Math.floor(Math.random() * array.lenght)];
+	};
+	//folkslag
+	const race = [
+		'Human',
+		'Insektoid',
+		'Mutant',
+		'Rese',
+		'Robot',
+		'Smallpeople',
+		'Underground',
+		'Ancient',
+	];
+
+	const randomRace = () => randomSelector(race);
+
+	//can't use class because it's a reserved keyword
+	const klass = ['Warrior', 'Magician', 'Thief'];
+
+	const randomKlass = () => randomSelector(klass);
+	//ödesmakt
+	const faith = ['Order', 'Chaos', 'Balance'];
+
+	const randomFaith = () => randomSelector(faith);
+	//styrka
+	const strenght = Math.floor(Math.random() * 18) + 3;
+	//smidighet
+	const dexterity = Math.floor(Math.random() * 18) + 3;
+	//fysik
+	const physics = Math.floor(Math.random() * 18) + 3;
+	//perception
+	const perception = Math.floor(Math.random() * 18) + 3;
+	//viljestyrka
+	const willpower = Math.floor(Math.random() * 18) + 3;
+	//utstrålning
+	const appearance = Math.floor(Math.random() * 18) + 3;
+	//startkapital
+	const startFunds = [
+		'A handful worthless pinecones and stones',
+		' 3 km',
+		'1 sm',
+		'5 sm',
+		'10 sm',
+		'15 sm',
+		'20 sm',
+		'30 sm',
+	];
+	const randomStartFunds = () => randomSelector(startFunds);
+
+	return (
+		<div>
+			<h1>Swedish roleplay-character randomizer</h1>
+			<p>Race: {randomRace()}</p>
+			<p>Class: {randomKlass()}</p>
+			<p>Faith: {randomFaith()}</p>
+			<p>Strenght: {strenght}</p>
+			<p>Dexterity: {dexterity}</p>
+			<p>Physics: {physics}</p>
+			<p>Perception: {perception}</p>
+			<p>Willpower: {willpower}</p>
+			<p>Appearance: {appearance}</p>
+			<p>Starting funds: {randomStartFunds()}</p>
+		</div>
+	);
+};
